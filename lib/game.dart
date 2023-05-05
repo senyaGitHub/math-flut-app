b282318d956b6d88abab85759ed72127bbb862fe
Arsenii_Selivanov <akselivanov2005@gmail.com>
2023-05-05 20:54:53 +0000

    A

--- lib/game.dart
+++ lib/game.dart
@@ -1,7 +1,14 @@
 import 'dart:math';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class Game extends StatefulWidget {
+  final bool showAnswers;
+  final int level;
+  const Game({Key? key, required this.showAnswers, required this.level})
+      : super(key: key);
+
   @override
   _GameState createState() => _GameState();
 }
@@ -13,6 +20,12 @@ class _GameState extends State<Game> {
   int answer = 0;
   int score = 0;
   int questionCount = 0;
+  int level = 1;
+
+  // Generate the possible answers with the correct answer and two other options within a certain range
+  int answer1 = 0;
+  int answer2 = 0;
+  int answer3 = 0;
 
   void generateQuestion() {
     num1 = Random().nextInt(12) + 1;
@@ -43,17 +56,37 @@ class _GameState extends State<Game> {
         answer = num1 ~/ num2;
         break;
     }
+
+    // Generate three possible answers with the correct answer and two other options within a certain range
+    answer1 = answer;
+    answer2 = answer;
+    while (answer2 == answer) {
+      answer2 = answer + Random().nextInt(10) + 1;
+    }
+    answer3 = answer;
+    while (answer3 == answer || answer3 < 0) {
+      answer3 = answer - Random().nextInt(10) - 1;
+    }
   }
 
   void checkAnswer(int userAnswer) {
     setState(() {
+      // Check the user's answer and update the score
       if (userAnswer == answer) score++;
-
+      // Increase the question count and check if the level has been cleared
       questionCount++;
       if (questionCount >= 10) {
         // If the user has answered 10 questions, navigate back to the GameMenu screen
+        bool clearedLevel = (score >= 7);
         Navigator.popUntil(context, ModalRoute.withName('/game-menu'));
+
+        // Increase level if user has cleared the level
+        if (clearedLevel) {
+          level = level + 1;
+          score = 0; // Reset score for next level
+        }
       } else {
+        // Generate a new question and start the timer again
         generateQuestion();
       }
     });
@@ -62,23 +95,11 @@ class _GameState extends State<Game> {
   @override
   void initState() {
     super.initState();
-    //Generate the first question
     generateQuestion();
   }
 
   @override
   Widget build(BuildContext context) {
-    // Generate three possible answers with the correct answer and two other options within a certain range
-    int answer1 = answer;
-    int answer2 = answer;
-    while (answer2 == answer) {
-      answer2 = answer + Random().nextInt(10) + 1;
-    }
-    int answer3 = answer;
-    while (answer3 == answer || answer3 < 0) {
-      answer3 = answer - Random().nextInt(10) - 1;
-    }
-
     // Shuffle the List to randomize the order of the possible answers
     List<int> answers = [answer1, answer2, answer3];
     answers.shuffle();
@@ -93,49 +114,55 @@ class _GameState extends State<Game> {
               style: const TextStyle(fontSize: 24.0),
             ),
             const SizedBox(height: 20.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                        const Color(0xff8c8c8c)),
-                    foregroundColor: MaterialStateProperty.all<Color>(
-                        Colors.black), // set the text color to black
+            widget.showAnswers
+                ? Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              const Color(0xff8c8c8c)),
+                          foregroundColor: MaterialStateProperty.all<Color>(
+                              Colors.black), // set the text color to black
+                        ),
+                        onPressed: () {
+                          checkAnswer(answers[0]);
+                        },
+                        child: Text('${answers[0]}'),
+                      ),
+                      ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              const Color(0xff8c8c8c)),
+                          foregroundColor: MaterialStateProperty.all<Color>(
+                              Colors.black), // set the text color to black
+                        ),
+                        onPressed: () {
+                          checkAnswer(answers[1]);
+                        },
+                        child: Text('${answers[1]}'),
+                      ),
+                      ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              const Color(0xff8c8c8c)),
+                          foregroundColor: MaterialStateProperty.all<Color>(
+                              Colors.black), // set the text color to black
+                        ),
+                        onPressed: () {
+                          checkAnswer(answers[2]);
+                        },
+                        child: Text('${answers[2]}'),
+                      ),
+                    ],
+                  )
+                : TextField(
+                    controller: TextEditingController(),
+                    onSubmitted: (value) {
+                      int userAnswer = int.tryParse(value) ?? -1;
+                      checkAnswer(userAnswer);
+                    },
                   ),
-                  onPressed: () {
-                    checkAnswer(answers[0]);
-                  },
-                  child: Text('${answers[0]}'),
-                ),
-                ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                        const Color(
-                            0xff8c8c8c)), // set the background color to gray
-                    foregroundColor: MaterialStateProperty.all<Color>(
-                        Colors.black), // set the text color to black
-                  ),
-                  onPressed: () {
-                    checkAnswer(answers[1]);
-                  },
-                  child: Text('${answers[1]}'),
-                ),
-                TextButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                        const Color(
-                            0xff8c8c8c)), // set the background color to gray
-                    foregroundColor: MaterialStateProperty.all<Color>(
-                        Colors.black), // set the text color to black
-                  ),
-                  onPressed: () {
-                    checkAnswer(answers[2]);
-                  },
-                  child: Text('${answers[2]}'),
-                ),
-              ],
-            ),
             const SizedBox(height: 20.0),
             Text(
               'Score: $score',
--- lib/game_menu.dart
+++ lib/game_menu.dart
@@ -14,8 +14,6 @@ class GameMenu extends StatelessWidget {
             ),
             const SizedBox(height: 20.0),
             playButton(context),
-            const SizedBox(height: 20.0),
-            optionsButton(context),
           ],
         ),
       ),
@@ -38,25 +36,6 @@ playButton(buildContext) {
         Navigator.pushNamed(buildContext, '/options');
       },
       child: const Text('play'), // set the button text
-    ),
-  );
-}
-
-optionsButton(buildContext) {
-  return SizedBox(
-    width: 200, // set the button width
-    height: 75, // set the button height
-    child: TextButton(
-      style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all<Color>(
-            const Color(0xff8c8c8c)), // set the background color to gray
-        foregroundColor: MaterialStateProperty.all<Color>(Colors
-            .black), // set t: MaterialStateProperty.all<Color>(Color(0xFFD9D9D9)), // set the text color to D9D9D9
-      ),
-      onPressed: () {
-        Navigator.pushNamed(buildContext, '/options');
-      },
-      child: const Text('options'), // set the button text
     ),
   );
 }
